37901f0bedc3a86d929cdb8aeb2497a4a26c9e37
bymse <egorstrelov@gmail.com>
2020-10-31 22:56:19 +0300

    Main for exercise check

---
+++ MathOps.Exercises/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MathOps.Exercises
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter exercise number: ");
+            var number = int.Parse(Console.ReadLine() ?? "1");
+
+            switch (number)
+            {
+                case 1:
+                    FirstExercise();
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        private static void FirstExercise()
+        {
+            Console.WriteLine("1. Dichotomy");
+
+            Console.Write("Enter task: ");
+            var numb = int.Parse(Console.ReadLine() ?? "1");
+            switch (numb)
+            {
+                case 1:
+                    DichotomyTaskRunner.Run();
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
