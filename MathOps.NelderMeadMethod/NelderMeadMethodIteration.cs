5e0b732e794ab1a6b4b643d52fc3b161fe790b56
bymse <egorstrelov@gmail.com>
2020-11-02 00:55:53 +0300

    Iteration model + test for Nelder-Mead method

---
+++ MathOps.NelderMeadMethod/NelderMeadMethodIteration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using MathOps.Utilities;
+
+namespace MathOps.NelderMeadMethod
+{
+    public class NelderMeadMethodIteration 
+    {
+        public int Iteration { get; set; }
+        
+        public Vector2 MinValueVertex { get; set; }
+        public Vector2 MaxValueVertex { get; set; }
+        public Vector2 BalancePointVertex { get; set; }
+        
+        public Vector2 ReflectedVertex { get; set; }
+        public Vector2? ExpandedVertex { get; set; }
+        public Vector2? ContractedVertex { get; set; }
+
+        public IReadOnlyList<Vector2> NextVertexList { get; set; }
+    }
+}
---
+++ MathOps.Tests/MultiDimensional/NelderMeadMethodExecutorTest.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using MathOps.NelderMeadMethod;
+using MathOps.Utilities;
+
+namespace MathOps.Tests.MultiDimensional
+{
+    public class
+        NelderMeadMethodExecutorTest : ExecutorTestBase<TwoDimensionalApproximateResult, NelderMeadMethodIteration>
+    {
+        protected override TwoDimensionalApproximateResult ExpectedResult => new TwoDimensionalApproximateResult
+        {
+            IterationsCount = 7,
+            Value = 0.0625M,
+            Arg = new Vector2(5, 6.25M)
+        };
+
+        protected override IReadOnlyList<NelderMeadMethodIteration> ExpectedIterationsList => new[]
+        {
+            new NelderMeadMethodIteration
+            {
+                Iteration = 0,
+                MaxValueVertex = new Vector2(10, 11),
+                MinValueVertex = new Vector2(8, 9),
+                BalancePointVertex = new Vector2(8, 10),
+                ReflectedVertex = new Vector2(6, 9),
+                ExpandedVertex = new Vector2(4, 8),
+                ContractedVertex = null,
+                NextVertexList = new[]
+                {
+                    new Vector2(8, 9),
+                    new Vector2(4, 8),
+                    new Vector2(8, 11),
+                }
+            },
+            new NelderMeadMethodIteration
+            {
+                Iteration = 1,
+                MaxValueVertex = new Vector2(8, 11),
+                MinValueVertex = new Vector2(4, 8),
+                BalancePointVertex = new Vector2(6, 8.5M),
+                ReflectedVertex = new Vector2(4, 6),
+                ExpandedVertex = new Vector2(2, 3.5M),
+                NextVertexList = new[]
+                {
+                    new Vector2(8, 9),
+                    new Vector2(4, 8),
+                    new Vector2(4, 6),
+                }
+            },
+            new NelderMeadMethodIteration
+            {
+                Iteration = 2,
+                MaxValueVertex = new Vector2(8, 9),
+                MinValueVertex = new Vector2(4, 6),
+                BalancePointVertex = new Vector2(4, 7),
+                ReflectedVertex = new Vector2(0, 5),
+                NextVertexList = new[]
+                {
+                    new Vector2(6, 7.5M),
+                    new Vector2(4, 7),
+                    new Vector2(4, 6),
+                }
+            },
+            new NelderMeadMethodIteration
+            {
+                Iteration = 3,
+                MaxValueVertex = new Vector2(6, 7.5M),
+                MinValueVertex = new Vector2(4, 6),
+                BalancePointVertex = new Vector2(4, 6.5M),
+                ReflectedVertex = new Vector2(2, 5.5M),
+                NextVertexList = new[]
+                {
+                    new Vector2(5, 6.75M),
+                    new Vector2(4, 6.5M),
+                    new Vector2(4, 6),
+                }
+            },
+            new NelderMeadMethodIteration
+            {
+                Iteration = 4,
+                MaxValueVertex = new Vector2(4, 6.5M),
+                MinValueVertex = new Vector2(5, 6.75M),
+                BalancePointVertex = new Vector2(4.5M, 6.375M),
+                ReflectedVertex = new Vector2(5, 6.25M),
+                ExpandedVertex = new Vector2(5.5M, 6.125M),
+                NextVertexList = new[]
+                {
+                    new Vector2(5, 6.75M),
+                    new Vector2(5, 6.25M),
+                    new Vector2(4, 6),
+                }
+            },
+            new NelderMeadMethodIteration
+            {
+                Iteration = 5,
+                MaxValueVertex = new Vector2(4, 6),
+                MinValueVertex = new Vector2(5, 6.25M),
+                BalancePointVertex = new Vector2(5, 6.5M),
+                ReflectedVertex = new Vector2(6, 7),
+                NextVertexList = new[]
+                {
+                    new Vector2(5, 6.5M),
+                    new Vector2(5, 6.25M),
+                    new Vector2(4.5M, 6.125M),
+                }
+            },
+            new NelderMeadMethodIteration
+            {
+                Iteration = 6,
+                MinValueVertex = new Vector2(5, 6.25M),
+                MaxValueVertex = new Vector2(4.5M, 6.125M),
+                BalancePointVertex = new Vector2(5, 6.375M),
+                ReflectedVertex = new Vector2(5.5M, 6.625M),
+                NextVertexList = new[]
+                {
+                    new Vector2(5, 6.375M),
+                    new Vector2(5, 6.25M),
+                    new Vector2(4.75M, 6.18M),
+                }
+            },
+        };
+
+        protected override TwoDimensionalApproximateResult ExecuteWithObserver(
+            Action<NelderMeadMethodIteration> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
