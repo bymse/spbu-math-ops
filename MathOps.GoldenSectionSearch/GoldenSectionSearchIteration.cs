e4563df9e21416e78ce9957b7d5c8ba8486dc016
bymse <egorstrelov@gmail.com>
2020-11-01 18:45:09 +0300

    Project for golden-section search + iteration model

---
+++ MathOps.GoldenSectionSearch/GoldenSectionSearchIteration.cs
@@ -0,0 +1,18 @@
+using MathOps.Utilities;
+
+namespace MathOps.GoldenSectionSearch
+{
+    public struct GoldenSectionSearchIteration
+    {
+        public int Iteration { get; set; }
+        
+        public decimal NextLeftArg { get; set; }
+        public decimal NextRightArg { get; set; }
+        
+        public decimal LeftArgResult { get; set; }
+        public decimal RightArgResult { get; set; }
+        
+        
+        public Boundaries NextBoundaries { get; set; }
+    }
+}
