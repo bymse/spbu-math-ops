4b58af2107d1317d57714b50b50f884a1d870c5c
bymse <egorstrelov@gmail.com>
2020-11-02 23:51:06 +0300

    Add second max value to iteration model

--- MathOps.NelderMeadMethod/NelderMeadMethodIteration.cs
+++ MathOps.NelderMeadMethod/NelderMeadMethodIteration.cs
@@ -3,18 +3,23 @@ using MathOps.Utilities;
 
 namespace MathOps.NelderMeadMethod
 {
-    public class NelderMeadMethodIteration 
+    public class NelderMeadMethodIteration
     {
         public int Iteration { get; set; }
-        
+
         public Vector2 MinValueVertex { get; set; }
+        public Vector2 SecondMaxValueVertex { get; set; }
         public Vector2 MaxValueVertex { get; set; }
         public Vector2 BalancePointVertex { get; set; }
-        
+
         public Vector2 ReflectedVertex { get; set; }
         public Vector2? ExpandedVertex { get; set; }
         public Vector2? ContractedVertex { get; set; }
 
         public IReadOnlyList<Vector2> NextVertexList { get; set; }
+
+
+        public void Deconstruct(out Vector2 min, out Vector2 max, out Vector2 balance) =>
+            (min, max, balance) = (MinValueVertex, MaxValueVertex, BalancePointVertex);
     }
 }
--- MathOps.Tests/MultiDimensional/NelderMeadMethodExecutorTest.cs
+++ MathOps.Tests/MultiDimensional/NelderMeadMethodExecutorTest.cs
@@ -8,23 +8,22 @@ namespace MathOps.Tests.MultiDimensional
     public class NelderMeadMethodExecutorTest
         : ExecutorTestBase<TwoDimensionalApproximateResult, NelderMeadMethodIteration>
     {
-
         private const decimal REFLECTION_COEFF = 1;
         private const decimal CONTRACTION_COEFF = 0.5M;
         private const decimal EXPANSION_COEFF = 2M;
         private const decimal BOUNDARY_COEFF = 0.2M;
-        
+
         private static decimal TestFunction(Vector2 args)
         {
             var (first, second) = args;
             return 4 * (first - 5) * (first - 5) + (second - 6) * (second - 6);
         }
 
-        private static readonly IList<Vector2> StartVertexes = new[]
+        private static readonly IReadOnlyList<Vector2> StartVertexes = new[]
         {
             new Vector2(8, 9),
             new Vector2(10, 11),
-            new Vector2(8, 11), 
+            new Vector2(8, 11),
         };
 
         protected override TwoDimensionalApproximateResult ExpectedResult => new TwoDimensionalApproximateResult
@@ -41,6 +40,7 @@ namespace MathOps.Tests.MultiDimensional
                 Iteration = 0,
                 MaxValueVertex = new Vector2(10, 11),
                 MinValueVertex = new Vector2(8, 9),
+                SecondMaxValueVertex = new Vector2(8, 11),
                 BalancePointVertex = new Vector2(8, 10),
                 ReflectedVertex = new Vector2(6, 9),
                 ExpandedVertex = new Vector2(4, 8),
@@ -57,6 +57,7 @@ namespace MathOps.Tests.MultiDimensional
                 Iteration = 1,
                 MaxValueVertex = new Vector2(8, 11),
                 MinValueVertex = new Vector2(4, 8),
+                SecondMaxValueVertex = new Vector2(8, 9),
                 BalancePointVertex = new Vector2(6, 8.5M),
                 ReflectedVertex = new Vector2(4, 6),
                 ExpandedVertex = new Vector2(2, 3.5M),
@@ -72,6 +73,7 @@ namespace MathOps.Tests.MultiDimensional
                 Iteration = 2,
                 MaxValueVertex = new Vector2(8, 9),
                 MinValueVertex = new Vector2(4, 6),
+                SecondMaxValueVertex = new Vector2(4, 8),
                 BalancePointVertex = new Vector2(4, 7),
                 ReflectedVertex = new Vector2(0, 5),
                 NextVertexList = new[]
@@ -86,6 +88,7 @@ namespace MathOps.Tests.MultiDimensional
                 Iteration = 3,
                 MaxValueVertex = new Vector2(6, 7.5M),
                 MinValueVertex = new Vector2(4, 6),
+                SecondMaxValueVertex = new Vector2(4, 7),
                 BalancePointVertex = new Vector2(4, 6.5M),
                 ReflectedVertex = new Vector2(2, 5.5M),
                 NextVertexList = new[]
@@ -100,6 +103,7 @@ namespace MathOps.Tests.MultiDimensional
                 Iteration = 4,
                 MaxValueVertex = new Vector2(4, 6.5M),
                 MinValueVertex = new Vector2(5, 6.75M),
+                SecondMaxValueVertex = new Vector2(4, 6),
                 BalancePointVertex = new Vector2(4.5M, 6.375M),
                 ReflectedVertex = new Vector2(5, 6.25M),
                 ExpandedVertex = new Vector2(5.5M, 6.125M),
@@ -115,6 +119,7 @@ namespace MathOps.Tests.MultiDimensional
                 Iteration = 5,
                 MaxValueVertex = new Vector2(4, 6),
                 MinValueVertex = new Vector2(5, 6.25M),
+                SecondMaxValueVertex = new Vector2(5, 6.75M),
                 BalancePointVertex = new Vector2(5, 6.5M),
                 ReflectedVertex = new Vector2(6, 7),
                 NextVertexList = new[]
@@ -129,13 +134,14 @@ namespace MathOps.Tests.MultiDimensional
                 Iteration = 6,
                 MinValueVertex = new Vector2(5, 6.25M),
                 MaxValueVertex = new Vector2(4.5M, 6.125M),
+                SecondMaxValueVertex = new Vector2(5, 6.5M),
                 BalancePointVertex = new Vector2(5, 6.375M),
                 ReflectedVertex = new Vector2(5.5M, 6.625M),
                 NextVertexList = new[]
                 {
                     new Vector2(5, 6.375M),
                     new Vector2(5, 6.25M),
-                    new Vector2(4.75M, 6.18M),
+                    new Vector2(4.75M, 6.1875M),
                 }
             },
         };
